e04cb2593fb5aac40f4a0ff3d31dd59f8c4c3e2d
Marlon <asherbenard254@gmail.com>
2023-10-07 05:36:34 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of our program
+ *
+ * Return: zero if function is correct
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
