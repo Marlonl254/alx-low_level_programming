ed93af3ef16a09dbc426ec39b2ed036cac4d217f
Marlon <asherbenard254@gmail.com>
2023-10-08 13:24:19 -0700

    Using for loop to Iterate

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
