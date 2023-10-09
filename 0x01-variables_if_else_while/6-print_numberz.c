08dd8c0ca29b18d3e19f537849875fe5a94a77bc
Marlon <asherbenard254@gmail.com>
2023-10-08 23:11:37 -0700

    Putchar whith int data type

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main func
+ *
+ * Return: zero when successful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
