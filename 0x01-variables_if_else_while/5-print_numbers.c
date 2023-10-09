446c4f2e7888fa4f4de20e5bac1d90cc5edb77b6
Marlon <asherbenard254@gmail.com>
2023-10-08 23:05:12 -0700

    For loops

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of our program
+ *
+ * Return: zero = successful execution
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
