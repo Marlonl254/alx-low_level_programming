fa8c29179aaef6f6a1ab3a27b035a4f4dfe55f5b
Marlon <asherbenard254@gmail.com>
2023-10-09 11:49:37 -0700

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,10 +9,16 @@
 int main(void)
 {
 	int i;
-	
-	for (i = 0; i <= 10; i++)
-		putchar(i + " ,");
-	putchar('$');
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
 	return (0);
 }
