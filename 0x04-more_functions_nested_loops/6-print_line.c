e4b1c7345d7dd363ab64220062a2b73f5cc63e11
Marlon <asherbenard254@gmail.com>
2023-10-13 14:02:09 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - Line is printed on terminal
+ * @n: Number of times the _ will be prnted
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
