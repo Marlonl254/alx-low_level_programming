0c122cbef106c2d4f83d6b98c99a52aefd959b7c
Marlon <asherbenard254@gmail.com>
2023-10-19 14:45:34 -0700

    Copy n number of string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - It copies a string
+ * @dest: The destination string
+ * @src: Source string
+ * @n: Number of string to copy
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
