86876ee917401ead2dd39211958f312f4b19f225
Marlon <asherbenard254@gmail.com>
2023-10-25 13:09:33 -0700

    factorial func

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,11 +10,7 @@ int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if (n == 0 || n == 1)
+	if (n == 0 || n == 1)
 		return (1);
-	else
-	{
-		n = n * factorial(n - 1);
-	}
-	return (n);
+	return (n * factorial(n - 1));
 }
