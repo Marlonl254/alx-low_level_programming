f13511fb16c31c4dab8b76359ac814ba83db73e5
Marlon <asherbenard254@gmail.com>
2023-10-25 15:20:23 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+int main_func(int n, int i);
+/*
+ * main_func - recurses to get the natural sqrt of a numm
+ * @n: number to cal sqrt
+ * @i: iterator
+ * Return: the resulting sqrt
+ */
+
+int main_func(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (main_func(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - returns natural sqrt of a num
+ *@n: number to get sqrt
+ *Return: sqrt
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (main_func(n,0));
+}
